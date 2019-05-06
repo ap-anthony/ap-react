7f255eecdb9e9e2a11a0236dcd8ba8ac58225dbd
Anthony Benavente <anthony@admissionpros.com>
2019-05-06 08:04:07 -0400

    started working on layout for home page

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -2,11 +2,55 @@ import React from 'react';
 import TokenContext from '../../contexts/token';
 
 import { Redirect } from 'react-router-dom';
+import Sidebar from '../../components/Sidebar/sidebar';
+import ApTopnav from '../../components/ApTopnav/aptopnav';
 
 export default class Home extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            // This will come from an API call at some point
+            menuLinks:{
+                "My Information": [
+                    {
+                        "menu_id": 138,
+                        "caption": "My Applicant Watch List",
+                        "id": "my-applicant-watch-list",
+                        "icon": null
+                    },
+                    {
+                        "menu_id": 137,
+                        "caption": "Edit Profile Information",
+                        "id": "edit-profile-information",
+                        "icon": null
+                    }
+                ]
+            },
+            collapsed: false
+        }
+
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    handleToggle(value) {
+        this.setState({
+            collapsed: value
+        });
+    }
+
+    componentDidMount() {
+        
+    }
+
+    renderMain(token) {
+        return (
+            <div className={"flex-r " + (this.state.collapsed ? 'collapsed' : '')}>
+                <Sidebar menuLinks={this.state.menuLinks} collapsed={this.state.collapsed}></Sidebar>
+                <ApTopnav onCollapseToggle={this.handleToggle} collapsed={this.state.collapsed}></ApTopnav>
+            </div>
+        );  
     }
 
     render() {
@@ -16,7 +60,7 @@ export default class Home extends React.Component {
                     if (value.token == null) {
                         return <Redirect to="/login" />;
                     } else {
-                        return <h1>Hello, {value.loggedInUser.unique_name}. You are the APEX Champion!</h1>;
+                        return this.renderMain(value.token);
                     }
                 }}
             </TokenContext.Consumer>
