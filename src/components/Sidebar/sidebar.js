1f34c441df414d1a9c2b05a6897c8118d6c158b2
Anthony Benavente <anthony@admissionpros.com>
2019-05-18 18:45:58 -0400

    Feat/styled components (#3)
    * added styled components for existing components
    
    * fixed failing test when no logo is found
    
    * styled the title section of the sidebar
    
    * forced snapshot updates with jest
    
    * fixed compile error

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,4 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+import { assets } from '../../theme';
+
+const SidebarContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+`;
+
+const SidebarLinksContainer = styled.div`
+    padding: 0 15px;
+`;
+
+const SidebarLink = styled.button`
+    width: 100%;
+    text-align: left;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: white;
+    font-size: 1em;
+    padding: 5px;
+    cursor: pointer;
+    font-size: 18px;
+`;
+
+const SidebarDropdown = styled.ul`
+    list-style-type: none;
+`;
+
+const SidebarHeader = styled.div`
+    height: 65px;
+    width: 100%;
+    display: flex;
+`;
+
+const SidebarHeaderLogo = styled.div`
+    width: 95px;
+    height: 100%;
+    background-image: url(${assets.ap_logo_white});
+`;
+
+const SidebarHeaderTitle = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+function SidebarDropdownItem(props) {
+    return (
+        <li key={props.key}>{props.children}</li>
+    );
+}
 
 export default class Sidebar extends React.Component {
 
@@ -12,15 +67,26 @@ export default class Sidebar extends React.Component {
 
     render() {
         return (
-            <div className="sidebar">
-                {Object.keys(this.props.menuLinks).map((currentKey) => {
-                    return (
-                        <div className="sidebar-link" key={currentKey}>
-                            <span className="sidebar-link-text">{currentKey}</span>
-                        </div>
-                    );
-                })}
-            </div>
+            <SidebarContainer>
+                <SidebarHeader>
+                    <SidebarHeaderLogo></SidebarHeaderLogo>
+                    <SidebarHeaderTitle>AdmissionPros</SidebarHeaderTitle>
+                </SidebarHeader>
+                <SidebarLinksContainer>
+                    {Object.keys(this.props.menuLinks).map((currentKey) => {
+                        return (
+                            <div key={currentKey}>
+                                <SidebarLink>{currentKey}</SidebarLink>
+                                <SidebarDropdown>
+                                    <SidebarDropdownItem>Example Link 1</SidebarDropdownItem>
+                                    <SidebarDropdownItem>Example Link 2</SidebarDropdownItem>
+                                    <SidebarDropdownItem>Example Link 3</SidebarDropdownItem>
+                                </SidebarDropdown>
+                            </div>
+                        );
+                    })}
+                </SidebarLinksContainer>
+            </SidebarContainer>
         );  
     }
 }
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const colors = {
+    teal1: '#3F5A5C',
+    teal2: '#588284',
+    teal3: '#8FB9BB',
+    gray1: '#333333',
+    gray2: '#4f4f4f',
+    gray3: '#828282',
+    gray4: '#bdbdbd',
+    gray5: '#e0e0e0',
+    gray6: '#f2f2f2',
+};
+
+export const assets = {
+    DIAGONAL_LINES_TEX: '/60-lines.png',
+    ap_logo_white: '/aplogo.png'
+};
