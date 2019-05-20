60fa28ec01119811277d46a9c16bf7eca1bae86e
Anthony Benavente <anthony@admissionpros.com>
2019-05-20 09:48:59 -0400

    added centralized location to place font awesome icons

---
+++ src/font-awesome-library.js
@@ -0,0 +1,38 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+    faStroopwafel,
+    faChartPie,
+    faTaxi,
+    faUserShield,
+    faFolderOpen,
+    faTasks,
+    faMoneyBillAlt,
+    faMailBulk,
+    faEnvelopeOpenText,
+    faCode,
+    faLaptopCode,
+    faDatabase,
+    faCog,
+    faSitemap,
+    faInfoCircle,
+    faChevronDown,
+    faChartLine,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStroopwafel);
+library.add(faChartPie);
+library.add(faTaxi);
+library.add(faUserShield);
+library.add(faFolderOpen);
+library.add(faTasks);
+library.add(faMoneyBillAlt);
+library.add(faMailBulk);
+library.add(faEnvelopeOpenText);
+library.add(faCode);
+library.add(faLaptopCode);
+library.add(faDatabase);
+library.add(faCog);
+library.add(faSitemap);
+library.add(faInfoCircle);
+library.add(faChevronDown);
+library.add(faChartLine);
