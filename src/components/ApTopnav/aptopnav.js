58949018560315c8e3fb4455d65eb0657030798a
Anthony Benavente <anthony@admissionpros.com>
2019-05-22 08:31:21 -0400

    added top icons to the nav

--- src/components/ApTopnav/aptopnav.js
+++ src/components/ApTopnav/aptopnav.js
@@ -1,20 +1,74 @@
 import React from 'react';
-import TokenContext from '../../contexts/token';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 
-import styles from './aptopnav.css';
 import styled from 'styled-components';
-import { assets } from '../../theme';
+import { assets, colors } from '../../theme';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const TopNavContainer = styled.div`
     display: flex;
     flex-direction: row;
     background-color: white;
     background-image: url(${assets.DIAGONAL_LINES_TEX});
-    padding: 4px;
+    padding: 0 4px;
     transition: transform 0.25s;
 `;
+
+const TopNavLeft = styled.div`
+    width: auto;
+`;
+
+const TopNavCenter = styled.div`
+    flex: 1;
+`;
+
+const TopNavRight = styled.div`
+    width: auto;
+`;
+
+function TopNavButton(props) {
+    let { icon, ...otherProps } = props;
+
+    if (!icon) {
+        icon = 'info-circle';
+    }
+
+    const TopNavButtonWrapper = styled.div`
+        width: 65px;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        font-size: 24px;
+        display: inline-block;
+    `;
+
+    const TopNavButtonIcon = styled.span`
+        text-shadow: 0 0 16px #fff, 0 0 0 #000, 1px 4px 6px transparent;
+        color: transparent;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        border-left: ${props.borderLeft ? props.borderLeft : '2px solid #e0e0e0' };
+        border-right: ${props.borderRight ? props.borderRight : 'none' };
+    `;
+
+    return (
+        <TopNavButtonWrapper onClick={otherProps.onClick}>
+            <TopNavButtonIcon>
+                <i className={'fas ' + icon}></i>
+            </TopNavButtonIcon>
+        </TopNavButtonWrapper>
+    );
+}
 
 export default class ApTopnav extends React.Component {
     
@@ -28,28 +82,27 @@ export default class ApTopnav extends React.Component {
         this.props.onCollapseToggle(!this.props.collapsed);
     }
 
+    onAlertsClick() {
+
+    }
+
     render() {
         return (
             <TopNavContainer>
-                <div className={styles.contentLeft}>
-                    <div className="menuToggle">
-                        <IconButton aria-label="Toggle Menu" onClick={this.toggleCollapse}>
-                            <Icon>menu</Icon>
-                        </IconButton>
-                    </div>
-                </div>
-                <div className={styles.contentCenter}></div>
-                <div className={styles.contentRight}>
-                    <TokenContext.Consumer>
-                        { ({token}) => {
-                            return (
-                                <div className="user-info">
-                                    {token.unique_name}
-                                </div>
-                            );
-                        }}
-                    </TokenContext.Consumer>
-                </div>
+                <TopNavLeft>
+                    <TopNavButton 
+                        onClick={this.toggleCollapse} 
+                        icon="fa-bars"
+                        borderLeft={false} />
+                </TopNavLeft>
+                <TopNavCenter></TopNavCenter>
+                <TopNavRight>
+                    <TopNavButton onClick={this.onAlertsClick} icon="fa-search" />
+                    <TopNavButton onClick={this.onAlertsClick} icon="fa-bullhorn" />
+                    <TopNavButton onClick={this.onAlertsClick} icon="fa-bookmark" />
+                    <TopNavButton onClick={this.onAlertsClick} icon="fa-glasses" />
+                    <TopNavButton onClick={this.onAlertsClick} icon="fa-bell" />
+                </TopNavRight>
             </TopNavContainer>
         );
     }
